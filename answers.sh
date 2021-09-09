958d6a2e3c0830ebb0336f2720e28005c6ec0c38
Tanay Naveen Athreya <tnathrey@data.cs.purdue.edu>
2021-09-09 13:12:57 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -3,43 +3,62 @@
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
+# Task 1
 
+mv /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Homework/MA165/HW2.txt /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Homework/MA165/HW1.txt
 
 # Task 2
 
+rm /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Memes/SpongeBob/money.jpeg
 
 # Task 3
 
+mv /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Memes/'Star Wars'/fafsa.jpg /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Memes/SpongeBob/  
 
 # Task 4
 
+rm -r /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Homework/CS191
 
 # Task 5
 
+mkdir /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/C++
 
 # Task 6
 
+mv /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Age.cpp /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/C++ 
 
 # Task 7
 
+cp -r /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Java /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Homework/CS180/Project_1
 
 # Task 8
 
+mv /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Count.c /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Goodbye.c /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/C
 
 # Task 9
 
+mv /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Name.py /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/Space.py /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Programs/'Python 3'
 
 # Task 10
 
+touch /homes/tnathrey/homework-1-tanay-athreya/ORIGIN/Homework/POL141/Essay.txt
 
 # Task 11
 
+ls ..
 
 # Task 12
-: '
-Type your vim/nano customizations below:
-
-
 
 '
+
+Type your vim/nano customizations below
+
+set autoindent expandtab
+set tabstop=2
+set softtabstop=2
+set shiftwidth=2
+set number
+colorscheme peachpuff
+
+'
+
